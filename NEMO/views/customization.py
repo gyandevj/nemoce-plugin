6d2ef1fa91fd9c9abe7f134dc94351e13449bd42
mrampant <mathieu.rampant@gmail.com>
2026-05-20 00:05:57 -0400

    - made category filter allow multiple selection - added customization (disabled by default) to show the category filter in calendar/tool control

--- NEMO/views/customization.py
+++ NEMO/views/customization.py
@@ -422,6 +422,7 @@ class CalendarCustomization(CustomizationBase):
         "calendar_outage_recurrence_limit": "90",
         "calendar_training_recurrence_limit": "90",
         "calendar_qualified_tools": "",
+        "calendar_tool_category_filter": "",
         "calendar_configuration_in_reservations": "",
         "calendar_status_bar_show_tool_reservation_policy": "enabled",
         "calendar_status_bar_show_tool_pinned_comments": "enabled",
